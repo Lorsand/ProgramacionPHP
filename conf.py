e76e65e79f3854e71d6a6a1b2a9c3bdaf4ea3dae
Armando Arce <armando.arce@gmail.com>
2014-02-20 09:33:33 -0600

    Problema con tíldes en títulos HTML

--- conf.py
+++ conf.py
@@ -104,10 +104,10 @@ pygments_style = 'sphinx'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Programación PHP'
+html_title = 'Desarrollo PHP'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Programación PHP'
+html_short_title = 'Desarrollo PHP'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
